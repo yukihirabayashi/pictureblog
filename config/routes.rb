4c689939aa28fd23072ba24f709416aceb2e7f70
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-03-22 14:00:21 +0000

    divise setting OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    root to: 'devise/sessions#new'
+#    root to: 'devise/sessions#new'
+    root to: 'pictures#index'
   end
 
 end
