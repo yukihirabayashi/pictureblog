31c9b197137ce7eedf090d22a58b5e9955ed9fc7
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-03-23 05:13:21 +0000

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable # 追記する
          
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
+#  devise_scope :user do
 #    root to: 'devise/sessions#new'
     root to: 'pictures#index'
-  end
+#  end
 
 end
