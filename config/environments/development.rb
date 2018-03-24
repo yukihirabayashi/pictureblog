c1b57f6056f4181b3e5454502c9771cc8a26636f
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-03-24 08:46:29 +0000

    devise FIX

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,11 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.action_mailer.default_url_options = { 
+    host: 'instaclone-yukihirabayashi.c9users.io',
+    port:8080}
+  
+  
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -51,4 +55,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.delivery_method = :letter_opener
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  resources :pictures
 #  devise_scope :user do
 #    root to: 'devise/sessions#new'
     root to: 'pictures#index'
 #  end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
 end
